1f5fa5d45740ef2f639dfc60e6eb7be3e6926a0e
SHAHAB UDDIN <shahab.82946@gmail.com>
2024-12-07 00:06:52 +0600

    toggle theme functionality done on home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
 import Banner from "../Components/Banner";
 import FeaturedMovies from "../Components/FeaturedMovies";
 
 
 const Home = () => {
+	const [theme, setTheme] = useState("light");
+	const toggleTheme = () => {
+		console.log('toggle')
+		// If the theme is light, change to dark, and vice versa
+		const newTheme = theme === "light" ? "dark" : "light";
+		setTheme(newTheme); // Update state
+		document.documentElement.setAttribute("data-theme", newTheme); // Set HTML attribute for DaisyUI
+	  };
 	return (
 		<div>
+			<div className="text-end">
+			<input onClick={toggleTheme} type="checkbox" className="toggle" defaultChecked />
+			</div>
+			<div>
 			<Banner></Banner>
+			</div>
 			<div>
 				<FeaturedMovies></FeaturedMovies>
 			</div>
